ebd901c1d0678174ea0f4b659c2f3941f6842875
Greg Honig <ghonig@Enterprise.local>
2009-07-09 23:23:56 -0500

    Added Note

---
+++ grails-app/domain/org/bworks/bworksdb/Note.groovy
@@ -0,0 +1,7 @@
+package org.bworks.bworksdb
+
+class Note {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/org/bworks/bworksdb/NoteTests.groovy
@@ -0,0 +1,17 @@
+package org.bworks.bworksdb
+
+import grails.test.*
+
+class NoteTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
